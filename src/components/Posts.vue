d163578558309eee2bcc6dbb05dfe59b27c33ad3
Theo Giovanna <theo.giovanna@gmail.com>
2018-04-09 17:42:05 +0200

    Temporary tag box

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,8 +1,37 @@
 <!-- Author: @GiovannaTheo -->
-<!-- This file controls the particuls that are on screen -->
+<!--This file contains the layout for the section last posts-->
 <template>
   <section>
     <div id="posts">
+      <v-layout>
+        <v-container style="max-height: 450px">
+          <v-flex xs6 sm4 md4>
+            <v-layout column>
+              <v-flex>
+                <v-card class="black--text" color="white" tile>
+                  <v-card-title primary class="title justify-center">Search for tags...</v-card-title>
+                </v-card>
+              </v-flex>
+              <v-flex d-flex style="max-height: 450px">
+                <v-card class="black--text" color="white" tile>
+                  <v-card-title primary-title>
+                    <h3 class="headline mb-0">University</h3>
+                    <div>
+                      here will go the future tags that a user picks
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn icon ripple>
+                      <v-icon color="blue">add</v-icon>
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-container>
+      </v-layout>
+
       <v-layout id="post"
                 column
                 justify-center
@@ -11,47 +40,51 @@
         <v-container style="max-height: 450px"
                      class="scroll-y"
                      id="scroll-target">
-          <v-flex xs12 sm8 md6>
-            <v-card>
-              <v-toolbar color="blue">
-                <v-toolbar-title style="color: white;">Latest posts</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-btn icon disabled>
-                  <v-icon color="white">search</v-icon>
-                </v-btn>
-              </v-toolbar>
-              <v-list two-line>
-                <template v-for="(item, index) in items">
-                  <v-subheader
-                    inset
-                    :key="item.header"
-                    v-if="item.header"
-                  >
-                    {{ item.header }}
-                  </v-subheader>
-                  <v-divider
-                    v-else-if="item.divider"
-                    inset
-                    :key="index"
-                  ></v-divider>
-                  <v-list-tile
-                    v-else
-                    avatar
-                    ripple
-                    :key="item.title"
-                  >
-                    <v-list-tile-avatar>
-                      <img :src="item.avatar">
-                    </v-list-tile-avatar>
-                    <v-list-tile-content>
-                      <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                      <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </template>
-              </v-list>
-            </v-card>
-          </v-flex>
+          <v-layout row wrap>
+            <v-flex d-flex xs6 sm4 md6 offset-xs0 offset-lg5>
+              <v-layout row wrap>
+                <v-flex d-flex>
+                  <v-card>
+                    <v-list two-line>
+                      <template v-for="(item, index) in items">
+                        <v-subheader
+                          inset
+                          :key="item.header"
+                          v-if="item.header"
+                        >
+                          {{ item.header }}
+                        </v-subheader>
+                        <v-divider
+                          v-else-if="item.divider"
+                          inset
+                          :key="index"
+                        ></v-divider>
+                        <v-list-tile
+                          v-else
+                          avatar
+                          ripple
+                          :key="item.title"
+                        >
+                          <v-list-tile-avatar>
+                            <img :src="item.avatar">
+                          </v-list-tile-avatar>
+                          <v-list-tile-content>
+                            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                          </v-list-tile-content>
+                          <v-list-tile-action>
+                            <v-btn icon ripple>
+                              <v-icon color="blue">open_in_new</v-icon>
+                            </v-btn>
+                          </v-list-tile-action>
+                        </v-list-tile>
+                      </template>
+                    </v-list>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
         </v-container>
       </v-layout>
     </div>
@@ -128,7 +161,8 @@ export default {
           subtitle:
             "<span class='text--primary'>Nancy</span> &mdash; Do you see what time it is?"
         }
-      ]
+      ],
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
     }
   }
 }
@@ -141,15 +175,6 @@ export default {
     height: 60%;
     text-align: center;
     position: fixed;
-    left: 45%;
-  }
-
-  #tags #tag{
-    bottom: 23%;
-    width: 100%;
-    height: 60%;
-    text-align: center;
-    position: fixed;
-    left: 0%;
+    left: 10%;
   }
 </style>
