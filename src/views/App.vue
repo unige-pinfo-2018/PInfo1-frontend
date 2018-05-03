0840570c70cc0eaaeab96cc87643858429350f26
Martin Jérémie <martin.jeremie42@gmail.com>
2018-05-03 18:06:31 +0200

    Présentation

--- src/views/App.vue
+++ src/views/App.vue
@@ -68,7 +68,7 @@ export default {
       count: 4,
       icons: ['fa fa-user', 'fa fa-comment', 'fa fa-search', 'fa fa-question'],
       list: ['/#/profile', '/#/posts', '/#/', '/#/ask'],
-      backgroundColor: '#2196F3',
+      backgroundColor: '#6F3219',
       color: '#ffffff',
       position: 'bottom-right',
       isOpenNewTab: false,
