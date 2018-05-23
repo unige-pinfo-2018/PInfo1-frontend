ad87fd9497518512cce832c9f70644fdf991ae15
Theo Giovanna <theo.giovanna@gmail.com>
2018-05-23 11:26:29 +0200

    Fix clickable menu
    Signed-off-by: Theo Giovanna <theo.giovanna@gmail.com>

--- src/views/App.vue
+++ src/views/App.vue
@@ -176,8 +176,8 @@ export default {
 /* CSS used for the social media links */
 
 #social {
-  bottom: 30px;
-  z-index: 0;
+  bottom: 10%;
+  /*z-index: 0;*/
   width: 100%;
   text-align: center;
   position: fixed;
